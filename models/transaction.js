e0f47a248cc271cc1be462ba20d60f13ae4d691e
Dylson Oliveira <dylsonuni@gmail.com>
2021-11-15 16:00:27 +0000

    moved all folders to the root

---
+++ models/transaction.js
@@ -0,0 +1,6 @@
+// Q for TA's: I'd like to quickly go over the role of the models
+// folder in the MVC structure. i.e,what is the models folder
+// responsible for.
+
+// Bringing in mongoose
+const mongoose = require("")
---
+++ routes/api.js
@@ -0,0 +1,2 @@
+const router = require("express").Router();
+const Transaction = require("../models/transaction.js");
