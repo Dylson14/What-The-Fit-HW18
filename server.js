71a9baa595ccb4a8f20d8128c04b2791e42aa984
Dylson Oliveira <dylsonuni@gmail.com>
2021-11-15 16:40:48 +0000

    added the middleware and mongoose connection for the server.s file

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// bringing in the express server
 const express = require("express");
 const mongoose = require("mongoose");
 
@@ -6,3 +5,20 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// what does the bottom bit of code do?
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
