e08410e9f470cd8f12ddb36d60706461c314644a
Dylson Oliveira <dylsonuni@gmail.com>
2021-11-16 16:28:41 +0000

    created the index.js file in models and exported the workout file

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
-const Transaction = require("../models/exercise.js");
+const Transaction = require("../models/Workout.js");
 
 // so we need to create a post request to create new exercises within an existing workout plan. The question is: Is there an existing workout plan
 // or do I need to create one?
 
-router.post("/api/exercise")
+// we will first work backwards starting from getting the workouts.
+// getting all the workouts
+router.get("/api/workouts", (req, res => {
+        db.Workout
+}))
