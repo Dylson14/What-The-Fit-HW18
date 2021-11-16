3152f14577c02679b790936e05b954ef1068cab2
Dylson Oliveira <dylsonuni@gmail.com>
2021-11-16 15:57:52 +0000

    added more parameters to my schema within models

--- models/exercise.js
+++ models/exercise.js
@@ -1,27 +1,60 @@
 // Q for TA's: I'd like to quickly go over the role of the models
 // folder in the MVC structure. i.e,what is the models folder
 // responsible for.
+// models: setting up data and organizinig how we want our data to look like
+// view:  what we give to end-users (client)
+// controllers: getting request and getting data from models
+
 
 // Bringing in mongoose
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const exerciseSchema = new Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: "Enter a name for exercise"
+const workoutSchema = new Schema({
+  day: {
+      type: Date,
+      default: Date.now
+  },
+  exercises: [
+    {
+      type: {
+          type: String,
+          trim: true,
       },
-      value: {
-        type: Number,
-        required: "Enter the amount of weight used in Kgs"
+      name: {
+          type: String,
+          trim: true,
       },
-      date: {
-        type: Date,
-        default: Date.now
+      duration: {
+          type: Number,
+      },
+      weight: {
+          type: Number,
+          default: 0
+      },
+      reps: {
+          type: Number,
+          default: 0
+      },
+      sets: {
+          type: Number,
+          default: 0
+      },
+      distance: {
+          type: Number,
+          default: 0
       }
-    });
+    }
+  ],
+  totalDuration: {
+    type: Number,
+    default: 0,
+  }
+})
+
+
+
 
     const exercise = mongoose.model("exercise", exerciseSchema);
 
--- routes/api.js
+++ routes/api.js
@@ -1,2 +1,7 @@
 const router = require("express").Router();
-const Transaction = require("../models/transaction.js");
+const Transaction = require("../models/exercise.js");
+
+// so we need to create a post request to create new exercises within an existing workout plan. The question is: Is there an existing workout plan
+// or do I need to create one?
+
+router.post("/api/exercise")
